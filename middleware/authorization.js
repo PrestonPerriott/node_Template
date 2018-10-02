78aad17072ef0397df8b03d4cd62d6a6acbf07c9
Preston Perriott <perriott@Prestons-MBP-2.fios-router.home>
2018-10-02 18:34:13 -0400

    Started Middleware
    Added route exclusion and token name

---
+++ middleware/authorization.js
@@ -0,0 +1,36 @@
+'use strict'
+
+var express  = require('express')
+var route = express.Router()
+//TODO: Build Mongoose DB 
+
+/// Routes we want to exclude 
+var routeExclusion = [
+    'api/login',
+    'api/register',
+    'api/refresh',
+    'api/version'
+]
+
+/// What we wanna do for all paths
+router.all('*', function(req, res, next) {
+    var willExclude = false
+    for (var x = 0; x <routeExclusion.length; x++) {
+        var item = routeExclusion[x]
+        /// if the route equals one of our exclusion urls
+        if (item === req.originalUrl) {
+            willExclude = true
+            break
+        }
+    }
+
+    if (willExclude) {
+        return next()
+    }
+
+    /// If this value isn't in the request throw an error
+    /// TODO: Make custom Error Class
+    if (!req.get('Authorization')) {
+        throw new Error("No Auth Token")
+    }
+})
---
+++ middleware/index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var express = require('express')
+var router = express.Router()
+
+router.use('api/', require('./authorization'))
+
+module.exports = router
